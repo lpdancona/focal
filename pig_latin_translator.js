991068275509dbd89432983b75b4413219cb8bcd
Leonardo Picchi Dancona <lpdancona@gmail.com>
2023-01-11 13:58:22 -0800

    adding folders

---
+++ pig_latin_translator.js
@@ -0,0 +1,16 @@
+var originalWords = process.argv.slice(2);
+var pigLatinWords = [];
+
+// console.log("originalWords is", originalWords);
+// console.log("pigLatinWords is", pigLatinWords);
+
+for (var i = 0; i < originalWords.length; i++) {
+  // console.log(translateToPigLatin(originalWords[i]));
+  pigLatinWords.push(translateToPigLatin(originalWords[i]));
+}
+
+console.log(pigLatinWords.join(" "));
+
+function translateToPigLatin(word) {
+  return word.slice(1, word.length) + word[0] + "ay";
+}
